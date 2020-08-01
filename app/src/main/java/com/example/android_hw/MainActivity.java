bd82bc9d93302dcc6500619199712d7b6c02cf52
lxyeah <1525135604@qq.com>
2020-08-01 18:20:24 +0800

    doc: add web

--- app/src/main/java/com/example/android_hw/MainActivity.java
+++ app/src/main/java/com/example/android_hw/MainActivity.java
@@ -3,6 +3,15 @@ package com.example.android_hw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getData();
+    }
+
+    private void getData() {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://beiyou.bytedance.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiService apiService = retrofit.create(ApiService.class);
+        Call<List<Video>> videos = apiService.getVideos();
+        videos.enqueue(new Callback<List<Video>>() {
+            @Override
+            public void onResponse(Call<List<Video>> call, Response<List<Video>> response) {
+                if (response.body() != null) {
+                    List<Video> videoList = response.body();
+                    for (int i = 0; i < videoList.size(); i++) {
+                        Log.d("retrofit", videoList.get(i).toString());
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Video>> call, Throwable t) {
+                Log.d("retrofit", t.getMessage());
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/android_hw/Video.java
@@ -0,0 +1,37 @@
+package com.example.android_hw;
+
+import androidx.annotation.NonNull;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Video {
+
+    @SerializedName("_id")
+    public String id;
+
+    @SerializedName("feedurl")
+    public String feed_url;
+
+    @SerializedName("nickname")
+    public String nickname;
+
+    @SerializedName("description")
+    public String description;
+
+    @SerializedName("likecount")
+    public int like_count;
+
+    @SerializedName("avatar")
+    public String avatar;
+
+    @NonNull
+    @Override
+    public String toString() {
+        return " _id: " + id +
+                " feedurl: " + feed_url +
+                " nickname: " + nickname +
+                " description: " + description +
+                " likecount: " + like_count +
+                "avatar: " + avatar;
+    }
+}
